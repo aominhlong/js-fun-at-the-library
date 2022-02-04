ef9007dd33113a15c6c6defaad6e8cb94c7433c6
Nicholas Ao <aominhlong@yahoo.com>
2022-02-03 17:17:52 -0700

    Initial Commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return (`The ${title}`);
 }
+
+function buildMainCharacter() {
+
+};
+
+function buildMainCharacter (name, age, pronouns) {
+  return mainCharacter = {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+  };
+};
+
+// *redo?
+function saveReview(addReviews, reviews) {
+  if (reviews.indexOf(addReviews) === -1) {
+    return reviews.push(addReviews);
+  } else (reviews.indexOf(addReviews) > -1)
+    return reviews;
+};
+
+
+function calculatePageCount(bookPageCount, bookTitle) {
+    return bookPageCount.length * 20;
+};
+
+
+function writeBook(bookTitle, bookCharacter, book) {
+  return writeBook = {
+    bookTitle: bookTitle,
+    mainCharacter: bookCharacter,
+    book: genre
+  };
+};
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
