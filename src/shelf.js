763496637297a490302595156f1ed48f40268b23
Nicholas Ao <aominhlong@yahoo.com>
2022-02-05 11:31:02 -0700

    add Changes

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,47 @@
+function shelfBook(nameBook, sciFiShelf) {
+  if (sciFiShelf.length <= 2) {
+  sciFiShelf.unshift(nameBook);
+  }
+};
+
+
+
+function unshelfBook(bookName, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === bookName) {
+ sciFiShelf.splice(i, 1);
+    }
+  }
+};
+
+
+ var arrayTitles = [];
+
+function listTitles(titles) {
+  for (var i = 0; i < titles.length; i++) {
+    arrayTitles.push(titles[i].title)
+  }
+  return arrayTitles.join(', ');
+};
+
+
+// sciFiShelf = array hyperion and dune objects
+// title = Hyperion
+// need .includes() ???
+
+function searchShelf(sciFiShelf, title) {
+  console.log(';lkasdjf;asjdf;jsal;df', sciFiShelf[i].title);
+  for (var i = 0; i < sciFiShelf.length; i++) {
+  }
+};
+
+// access the array -> object -> titles
+// includes() access
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
